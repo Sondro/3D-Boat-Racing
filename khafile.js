d5f5821c7ef43e67fd4bed2424a353181669e68e
Sondro <memoiran@hotmail.com>
2018-03-09 04:29:37 -0800

    Add files via upload

---
+++ khafile.js
@@ -0,0 +1,8 @@
+let project = new Project('KhajakTest');
+project.addLibrary('Khajak');
+project.addLibrary('haxebullet');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+project.addShaders('Libraries/Khajak/Sources/khajak/Shaders/**');
+project.addShaders('Sources/Shaders/**');
+resolve(project);
